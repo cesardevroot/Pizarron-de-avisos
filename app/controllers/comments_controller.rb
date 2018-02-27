b9d63d2ffb9275386b95007c394b8a6cb9110231
cesardevroot <cesar.dev.root@gmail.com>
2018-02-27 13:42:49 -0300

    se actualiza controller comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
 
+	before_action :find_message
+	before_action :find_comment, only: [:edit, :update,:destroy]
+
+
+
 def create
-  @message = Message.find(params[:message_id])
+
   @comment = @message.comments.create(comment_params)
   @comment.user_id = current_user.id
 
@@ -14,12 +19,15 @@ end
 
 
 def edit
-	@message = Message.find(params[:message_id])
-	@comment = @message.comments.find(params[:id])
 end
 
 
 def update
+  if @comment.update(comment_params)
+  	redirect_to message_path(@message)
+  else
+  	render 'edit'
+  end
 end
 
 
@@ -30,8 +38,15 @@ def comment_params
 	params.require(:comment).permit(:content)
 end
 
+def find_message
+	@message = Message.find(params[:message_id])
+end
 
 
+def find_comment
+
+    @comment = @message.comments.find(params[:id])
+end
 
 end
 
